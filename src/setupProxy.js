dd6b437059b6dc7a08b2b3aa10f9b9f31cd00cb6
ER-EPR <38782737+ER-EPR@users.noreply.github.com>
2023-11-24 18:42:34 +0800

    Update setupProxy.js
    Signed-off-by: ER-EPR <38782737+ER-EPR@users.noreply.github.com>

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    'http://ai.savorcare.com',
     createProxyMiddleware({
       target: 'http://ai.savorcare.com',
       changeOrigin: true,
